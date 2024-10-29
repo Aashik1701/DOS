637f2769180314166b0fe6bafd324e22e9489293
Aashik1701 <md1704ashik@gmail.com>
2024-10-29 23:59:50 +0530

    update

--- src/components/Pages/Hero/gg/NotFound.jsx
+++ src/components/Pages/Hero/gg/NotFound.jsx
@@ -25,7 +25,7 @@ const NotFound = () => {
           onClick={() => router.push('/')}
           className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
         >
-          Go Back Home
+          Go Back Home.
         </button>
       )}
     </div>
